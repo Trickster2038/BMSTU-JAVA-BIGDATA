ca0820e41f9fd47bf33e2b090a67a28d59467775
Sergey <fzastahov@gmail.com>
2024-03-24 21:21:18 +0300

    9-2-1

---
+++ task_9_2_1/src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        System.out.println("=== Stram API work example ===\n\n");
+
+        ArrayList<String> st = new ArrayList<>();
+        st.add("st1");
+        st.add("st2");
+        st.add("st3");
+        st.add("apple");
+        st.add("st5");
+        st.add("st5");
+        st.add("st6");
+
+        String rg1 = ".*app.*";
+        String rg2 = ".*pineapple.*";
+
+        System.out.printf("ArrayList: %s\n", st);
+        System.out.printf("ArrayList[sorted, uniq]: %s\n", st
+                .stream().collect(Collectors.toSet())
+                .stream().collect(Collectors.toList())
+                .stream().sorted()
+                .collect(Collectors.toList()));
+    }
+}
+
