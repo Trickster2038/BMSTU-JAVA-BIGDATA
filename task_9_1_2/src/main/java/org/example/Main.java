124bb7f64738173746fb8662e708b7f0e6a32622
Sergey <fzastahov@gmail.com>
2024-03-24 21:10:34 +0300

    9-1-2

--- task_9_1_2/src/main/java/org/example/Main.java
+++ task_9_1_2/src/main/java/org/example/Main.java
@@ -17,13 +17,17 @@ public class Main {
         st.add("st1");
         st.add("st2");
         st.add("st3");
-        st.add("st4");
+        st.add("apple");
         st.add("st5");
         st.add("st6");
 
+        String rg1 = ".*app.*";
+        String rg2 = ".*pineapple.*";
+
         System.out.printf("ArrayList: %s\n", st);
-        System.out.printf("ArrayList[last]: %s\n", st.stream().reduce((first, second) -> second).orElse(null));
-        System.out.printf("ArrayList[2]: %s\n", st.stream().skip(2).findFirst().get());
+        System.out.printf("ArrayList[first]: %s\n", st.stream().findFirst().get());
+        System.out.printf("ArrayList[regex='%s']: %b\n", rg1, st.stream().anyMatch(row -> row.matches(rg1)));
+        System.out.printf("ArrayList[regex='%s']: %b\n", rg2, st.stream().anyMatch(row -> row.matches(rg2)));
     }
 }
 
